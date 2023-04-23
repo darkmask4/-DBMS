e026e2ef827bfa65a6a6afbb9eee2ee16b624c89
darkmask4 <131189232+darkmask4@users.noreply.github.com>
2023-04-23 17:44:52 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Format.sqlFormat("CREATE   TABLE  S   ;   ");
+		Create.Ifcreate_table(Format.sqlFormat("CREATE   TABLE  S ( classno char(6),classname char(30) );   "));
 	}
 
 }
