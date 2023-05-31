9ac8885a0ee5f2ddd90e1cf7117207f8f6689e8d
heligreater <128032087+heligreater@users.noreply.github.com>
2023-05-31 09:56:25 +0800

    Update User.java
    最终版

--- User.java
+++ User.java
@@ -1,78 +1,58 @@
 import java.io.*;
-import java.util.*;
-import javax.print.DocFlavor.STRING;
  public class User{
-   String id;
-   String password;
    public static String access;
 
-   User(String id, String password){
-     this.id = id;
-     this.password = password;
-   }
-
-    public String getId(){
-      return id;
-    }
-    
-    public String getPassword(){
-      return password;
-    }
-
-    //将用户信息写入文件
-    public void writeToFile(){
+    //注册时将用户信息写入文件
+    public boolean Register(String id, String password){
       try{
         File file = new File("user.txt");
+        FileReader fr = new FileReader(file);
+        try (BufferedReader br = new BufferedReader(fr)) {
+          String line = null;
+          while((line = br.readLine()) != null){
+            String[] info = line.split(" ");
+            //判断用户名是否已存在
+            if(info[0].equals(id)){
+              System.out.println("该用户已存在");
+              return false;
+            }
+          }
+          br.close();
+        }
+        fr.close();
         FileWriter fw = new FileWriter(file, true);
         BufferedWriter bw = new BufferedWriter(fw);
-        bw.write(id + " " + password);
+        bw.write(id + " " + password+" "+"1000000");
         bw.newLine();
         bw.close();
         fw.close();
       }catch(IOException e){
         e.printStackTrace();
       }
+      return true;
     }
-
-    //从文件中读取用户信息
-    public static ArrayList<User> readFromFile(String id, String password){
-      ArrayList<User> users = new ArrayList<User>();
+    //登入验证
+    public static boolean login(String id,String password){
       try{
         File file = new File("user.txt");
         FileReader fr = new FileReader(file);
-        BufferedReader br = new BufferedReader(fr);
-        String line = null;
-        while((line = br.readLine()) != null){
-          String[] info = line.split(" ");
-          User user = new User(info[0], info[1]);
-          users.add(user);
+        try (BufferedReader br = new BufferedReader(fr)) {
+          String line = null;
+          while((line = br.readLine()) != null){
+            String[] info = line.split(" ");
+            //判断用户名和密码是否正确
+            if(id.equals(info[0]) && password.equals(info[1])){
+              access = info[2];
+              return true;
+            }
+          }
+          br.close();
         }
-        br.close();
         fr.close();
       }catch(IOException e){
         e.printStackTrace();
       }
-      return users;
-    }
-
-    public static boolean login(String id,String password){
-      ArrayList<User> users = readFromFile(id, password);
-      for(User user : users){
-        if(user.getId().equals(id) && user.getPassword().equals(password)){
-          return true;
-        }
-      }
       return false;
-    }
-    public static boolean register(String id){
-      //如果用户名已存在，返回false
-      ArrayList<User> users = readFromFile(id, "");
-      for(User user : users){
-        if(user.getId().equals(id)){
-          return false;
-        }
-      }
-      return true;
     }
     
     
